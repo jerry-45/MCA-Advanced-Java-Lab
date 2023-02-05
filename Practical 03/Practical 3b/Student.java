18afaddefe384636b94bd921e03b1f592487ffee
jerry-45 <vishwa.atul16@gmail.com>
2023-02-05 19:35:03 +0530

    updated some files

--- Practical 03/Practical 3b/Student.java
+++ Practical 03/Practical 3b/Student.java
@@ -1,31 +1,45 @@
-public class Student {
+public class Student 
+{
     int rollno;
     String name;
     Double percentage;
-    public Student(int rollno, String name, Double percentage) {
-    this.rollno = rollno;
-    this.name = name;
-    this.percentage = percentage;
-    }
-    public Student()
+
+    public Student(int rollno, String name, Double percentage) 
     {
-    }
-    public int getRollno() {
-    return rollno;
-    }
-    public void setRollno(int rollno) {
-    this.rollno = rollno;
-    }
-    public String getName() {
-    return name;
-    }
-    public void setName(String name) {
+        this.rollno = rollno;
         this.name = name;
-        }
-        public Double getPercentage() {
-        return percentage;
-        }
-        public void setPercentage(Double percentage) {
         this.percentage = percentage;
-        }
+    }
+
+    public Student(){}
+
+    public int getRollno() 
+    {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) 
+    {
+        this.rollno = rollno;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+        
+    public Double getPercentage() 
+    {
+        return percentage;
+    }
+
+    public void setPercentage(Double percentage) 
+    {
+        this.percentage = percentage;
+    }
 }
--- Practical 03/Practical 3b/setExample.java
+++ Practical 03/Practical 3b/setExample.java
@@ -5,107 +5,119 @@
  * d. Search the specified item in the set
  */
 
- import java.util.HashSet;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.function.Predicate;
 
-public class setExample {
+public class setExample 
+{
     public static void main(String []args)
     {
-    String ans=null;
-    int rn;
-    String sn;
-    double per;
-    Scanner sc=new Scanner(System.in);
-    Student s=new Student();
-    int choice;
-//Creating set of students
-HashSet<Student>studentSet=new HashSet<Student>();
-Iterator i;
-//Creating Students
+        String ans=null;
+        int rn;
+        String sn;
+        double per;
+    
+        Scanner sc=new Scanner(System.in);
+        Student s=new Student();
+        
+        int choice;
+        
+        //Creating set of students
+        HashSet<Student>studentSet=new HashSet<Student>();
+        Iterator i;
 
-do
-{
-System.out.println("Menu");
-System.out.println("1.Adding student");
-System.out.println("2.Removing student");
-System.out.println("3.Search student");
-System.out.println("4.List all students");
-System.out.println("5.Exit");
-System.out.print("Enter your choice(1..4)");
-choice=sc.nextInt();
-int flag=0;
-switch(choice)
-{
-case 1:
-System.out.print("Enter rollno");
-rn=sc.nextInt();
-System.out.print("Enter name");
-sn=sc.next();
-System.out.print("Enter percentage");
-per=sc.nextDouble();
-s=new Student(rn,sn,per);
-studentSet.add(s);
-break;
-case 2:
-System.out.print("Enter roll no to remove element");
-rn=sc.nextInt();
-i=studentSet.iterator();
-while(i.hasNext())
-{
-s=(Student)i.next();
-if(s.getRollno()==rn)
-{
-    i. remove();
-    System.out.println("Element is successfully removed");
-    break;
-    }
-    else
-    {
-    System.out.println("Rollno="+rn);
-    }
-    }
-    break;
+        //Creating Students
+        do
+        {
+            System.out.println("Menu");
+            System.out.println("1.Adding student");
+            System.out.println("2.Removing student");
+            System.out.println("3.Search student");
+            System.out.println("4.List all students");
+            System.out.println("5.Exit");
+            System.out.print("Enter your choice(1..4)");
+            
+            choice=sc.nextInt();
+            int flag=0;
 
-    case 3:
-System.out.println("Enter the name of student");
-sn=sc.next();
-i=studentSet.iterator();
-if(!i.hasNext())
-{
-System.out.println("List is empty");
-}
-while(i.hasNext())
-{
-s=(Student)i.next();
-if(s.getName().equals(sn))
-{
-System.out.println("Record found");
-System.out.println(s.getRollno()+" "+s.getName()+" "+s.getPercentage());
-flag=1;
-break;
-}
-if(flag!=1)
-{
-System.out.println("Record not found search again");
-}
-}
-break;
-case 4:
-System.out.println("Rollno"+"\t"+"Name"+"\t"+"Percentage");
-studentSet.forEach((s1) -> {
-System.out.println(s1.getRollno()+" "+s1.getName()+" "+s1.getPercentage());
-});
-break;
-case 5:
-System.exit(0);
-break;
-}
-//adding student in HashSet
-System.out.print("Do you wish to continue(y/n)");
-ans=sc.next();
-}while(ans.equals("Y")||ans.equals("y"));
-//reteriving the student from set
-}
+            switch(choice)
+            {
+                case 1:
+                    System.out.print("Enter rollno");
+                    rn=sc.nextInt();
+                    System.out.print("Enter name");
+                    sn=sc.next();
+                    System.out.print("Enter percentage");
+                    per=sc.nextDouble();
+                    s=new Student(rn,sn,per);
+                    studentSet.add(s);
+                    break;.
+
+                case 2:
+                    System.out.print("Enter roll no to remove element");
+                    rn=sc.nextInt();
+                    i=studentSet.iterator();
+                    while(i.hasNext())
+                    {
+                        s=(Student)i.next();
+                        if(s.getRollno()==rn)
+                        {
+                            i.remove();
+                            System.out.println("Element is successfully removed");
+                            break;
+                        }
+                        else
+                        {
+                            System.out.println("Rollno="+rn);
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Enter the name of student");
+                    sn=sc.next();
+                    i=studentSet.iterator();
+                    if(!i.hasNext())
+                    {
+                        System.out.println("List is empty");
+                    }
+                    while(i.hasNext())
+                    {
+                        s=(Student)i.next();
+                        if(s.getName().equals(sn))
+                        {
+                            System.out.println("Record found");
+                            System.out.println(s.getRollno()+" "+s.getName()+" "+s.getPercentage());
+                            flag=1;
+                            break;
+                        }
+
+                        if(flag!=1)
+                        {
+                            System.out.println("Record not found search again");
+                        }
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Rollno"+"\t"+"Name"+"\t"+"Percentage");
+                    studentSet.forEach((s1) -> {
+                        System.out.println(s1.getRollno()+" "+s1.getName()+" "+s1.getPercentage());
+                    });
+                    break;
+
+                case 5:
+                    System.exit(0);
+                    break;
+            }
+
+            //adding student in HashSet
+            System.out.print("Do you wish to continue(y/n)");
+            ans=sc.next();
+        }
+        while(ans.equals("Y")||ans.equals("y"));
+        //reteriving the student from set
+    }
 }
